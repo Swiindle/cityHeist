423ea8408ee65a23c97d2edf91108cd74cf39360
Mark <18markl@Mark-mac.local>
2019-06-28 22:57:00 +0800

    dice now works

--- Dice.java
+++ Dice.java
@@ -1,24 +1,36 @@
 import javax.swing.*;
-public class Dice
+import java.awt.*;
+import java.awt.event.*; // #includes action listener
+
+
+public class Dice implements ActionListener
 {
     private JFrame frame = new JFrame("Dice");                  // frame
     private JPanel panel = new JPanel();                        // panel
-   // private BorderLayout layout = new BorderLayout(1,1);
-    private JButton b = new JButton();
+    private GridLayout layout = new GridLayout(1,0);
+    private JButton b = new JButton("HELLO");
+    
     public void open()
     {
         //FRAME
-        frame.setSize(500,500);
+        frame.setSize(100,100);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // frame closes when close
         
         //PANEL
         frame.setContentPane(panel);                            // connects frame and panel
-        //panel.setLayout(layout);                                // connects the panel and the layout
-        
+        panel.setLayout(layout);                                // connects the panel and the layout
+        panel.add(b);
+        b.addActionListener(this);
         frame.setVisible(true);                                 // makes frame visible
     }
     public int roll()
     {
-        return (int)Math.random() * 6 + 1;
+        int x = (int)(Math.random() * 6 + 1);
+        b.setText(Integer.toString(x));
+        return x;
+    }
+    public void actionPerformed(ActionEvent action)
+    {
+        roll();
     }
 }
